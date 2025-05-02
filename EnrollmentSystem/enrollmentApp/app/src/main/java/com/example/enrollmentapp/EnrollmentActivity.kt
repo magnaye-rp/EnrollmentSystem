153aeca19b5d1498c9551071b018ff5a502c9a6b
Ryan Paulo Magnaye <magnaye.rp@gmail.com>
2025-05-02 19:45:09 +0800

    finished framework for android app

---
+++ EnrollmentSystem/enrollmentApp/app/src/main/java/com/example/enrollmentapp/EnrollmentActivity.kt
@@ -0,0 +1,29 @@
+package com.example.enrollmentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.example.enrollmentapp.MainActivity
+import com.example.enrollmentapp.databinding.ActivityEnrollmentBinding
+import androidx.appcompat.app.AppCompatActivity
+
+
+class EnrollmentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEnrollmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val student_id = intent.getStringExtra("student_id")
+        val student_name = intent.getStringExtra("student_name")
+
+        Toast.makeText(this, "Welcome $student_name!", Toast.LENGTH_LONG).show()
+
+        binding = ActivityEnrollmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Button click listener
+
+    }
+}
--- EnrollmentSystem/enrollmentApp/app/src/main/java/com/example/enrollmentapp/MainActivity.kt
+++ EnrollmentSystem/enrollmentApp/app/src/main/java/com/example/enrollmentapp/MainActivity.kt
@@ -11,8 +11,12 @@ import java.io.IOException
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 import android.content.Intent
+import com.example.enrollmentapp.LoginActivity
+import com.example.enrollmentapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +26,16 @@ class MainActivity : AppCompatActivity() {
         val student_name = intent.getStringExtra("student_name")
 
         Toast.makeText(this, "Welcome $student_name!", Toast.LENGTH_LONG).show()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Button click listener
+        binding.floatingActionButton.setOnClickListener {
+
+            val intent = Intent(this@MainActivity, EnrollmentActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
